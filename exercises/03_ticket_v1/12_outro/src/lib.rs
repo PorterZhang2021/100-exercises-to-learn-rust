e797c504686306e43fc60ce9fc5e444bf22a4602
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-01-16 15:02:45 +0800

    fix: finish ticket_v1-outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,80 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order{
+
+        Self::valid_empty(&product_name, "product_name");
+        Self::valid_size(&product_name, "product_name");
+
+        Self::valid_value(quantity, "quantity");
+        Self::valid_value(unit_price, "unit_price");
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        Self::valid_empty(&product_name, "product_name");
+        Self::valid_size(&product_name, "product_name");
+        self.product_name = product_name
+    }
+
+    pub fn set_quantity(&mut self, quantity: i32) {
+        Self::valid_value(quantity, "quantity");
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: i32) {
+        Self::valid_value(unit_price, "unit_price");
+        self.unit_price = unit_price;
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+
+    fn valid_empty(param: &String, param_name: &str) {
+        if param.is_empty() {
+            panic!("{param_name} can't be empty.")
+        }
+    }
+
+
+    fn valid_size(param:&String, param_name: &str) {
+        if param.capacity() >= 300 {
+            panic!("{param_name} cannot be longer than 300 bytes.")
+        }
+    }
+
+    fn valid_value(param: i32, param_name: &str) {
+        if param <= 0 {
+            panic!("{param_name} must be strictly greater than zero.")
+        }
+    }
+
+}
