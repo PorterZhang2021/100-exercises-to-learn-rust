f64064cac402c25907b99aeb9dc9c240ffee71d7
PorterZhang2021 <porterzhang2021@outlook.com>
2025-01-21 21:08:31 +0800

    fix: finish ticket_v2-if_let

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,25 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        if let Shape::Circle { radius } = self {
+            return *radius;
+        } else {
+            panic!("Not a circle");
+        }
+
+        if let Shape::Square { border } = self {
+            return *border;
+        } else {
+            panic!("Not a square");
+        }
+
+        if let Shape::Rectangle { width, height } = self {
+            return width * height;
+        } else {
+            panic!("Not a rectangle");
+        }
+
+        return -1.0;
     }
 }
 
